cd1b7f2affb113d5a913f15ca09bea3c4dcb65a4
edman3d <ejsmajestic@gmail.com>
2025-10-12 11:14:08 -0400

    enable some stuff back

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,17 @@
-// import "dotenv/config";
-// // import dotenv from "dotenv";
-// import express, { NextFunction, Request, Response } from "express";
-// import notesRoutes from "./routes/notes";
-// import userRoutes from "./routes/users";
-// import morgan from "morgan";
-// import createHttpError, { isHttpError } from "http-errors";
-// import session from "express-session";
+import "dotenv/config";
+// import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoutes from "./routes/notes";
+import userRoutes from "./routes/users";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
 import env from "./util/validateEnv";
-// import MongoStore from "connect-mongo";
-// import { requiresAuth } from "./middleware/auth";
-// import cors from "cors";
+import MongoStore from "connect-mongo";
+import { requiresAuth } from "./middleware/auth";
+import cors from "cors";
 
-// const app = express();
+const app = express();
 
 // console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
 // console.log("process.env.REACT_APP_MONOREPO_FRONTEND_URL: " + process.env.REACT_APP_MONOREPO_FRONTEND_URL);
@@ -32,9 +32,9 @@ import env from "./util/validateEnv";
 // app.use(cors(corsOptions));
 // // app.use(cors());
 
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 
-// app.use(express.json());
+app.use(express.json());
 
 // // Cookies stop working as soon as we stop using the localhost domain
 // app.use(session({
@@ -50,43 +50,43 @@ import env from "./util/validateEnv";
 //     }),
 // }));
 
-// console.log('session middleware configured');
+console.log('session middleware configured');
 
-// app.use("/api/users", userRoutes);
-// // app.use("/api/notes", requiresAuth, notesRoutes);
-// app.use("/api/notes", notesRoutes);
+app.use("/api/users", userRoutes);
+// app.use("/api/notes", requiresAuth, notesRoutes);
+app.use("/api/notes", notesRoutes);
 
-// app.use((req, res, next) => {
-//     next(createHttpError(404, "Endpoint not found"));
-// });
+app.use((req, res, next) => {
+    next(createHttpError(404, "Endpoint not found"));
+});
 
-// console.log('post 404 middleware');
+console.log('post 404 middleware');
 
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-//     console.error(error);
-//     let errorMessage = "An unknown error occurred";
-//     let statusCode = 500;
-//     if (isHttpError(error)) {
-//         statusCode = error.status;
-//         errorMessage = error.message;
-//     }
-//     res.status(statusCode).json({ error: errorMessage });
-// });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
 
 // console.log('post 500 middleware');
 
 // export default app;
 
 // Use "type: module" in package.json to use ES modules
-import express from 'express';
-const app = express();
+// import express from 'express';
+// const app = express();
 
 
-// Define your routes
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Express on Vercel!' });
-});
+// // Define your routes
+// app.get('/', (req, res) => {
+//     res.json({ message: 'Hello from Express on Vercel!' });
+// });
 
 
 
