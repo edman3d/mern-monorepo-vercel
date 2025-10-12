c24ed0459b57f90247312671983c354dd237a128
edman3d <ejsmajestic@gmail.com>
2025-10-12 12:04:20 -0400

    unify server and app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,22 @@
-import app from "./app";
+import "dotenv/config";
+// import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoutes from "./routes/notes";
+import userRoutes from "./routes/users";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
 import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
+import { requiresAuth } from "./middleware/auth";
+import cors from "cors";
 import mongoose from "mongoose";
+const app = express();
+
+console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
+console.log("process.env.REACT_APP_MONOREPO_FRONTEND_URL: " + process.env.REACT_APP_MONOREPO_FRONTEND_URL);
+console.log("env.SESSION_SECRET", env.SESSION_SECRET);
+console.log("env.MONGODB_URI", env.MONGODB_URI);
 
 const port = env.PORT;
 
@@ -9,11 +25,54 @@ console.log('trying to connect to mongo at: ' + env.MONGODB_URI);
 mongoose.connect(env.MONGODB_URI)
     .then(() => {
         console.log("Mongoose connected");
-        app.listen(port, () => {
-            console.log("Server running on port: " + port);
-        });
+        // app.listen(port, () => {
+        //     console.log("Server running on port: " + port);
+        // });
     })
-    .catch(console.error);
+    .catch(err => console.error('MongoDB connection error:', err));
 
 
 console.log('post-connection hello');
+
+// const corsOptions = {
+//     origin: [env.REACT_APP_MONOREPO_FRONTEND_URL, 'http://localhost:3000'], // Allow requests only from these origins
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // Allow cookies, if your application uses them
+//     optionsSuccessStatus: 204,
+//     // headers: 'Content-Type, Authorization, Content-Length, X-Requested-With',
+// };
+// console.log('CORS origins: ', corsOptions.origin.join(', '));
+// app.use(cors(corsOptions));
+// // app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+console.log('session middleware configured');
+
+app.use("/api/users", userRoutes);
+// app.use("/api/notes", requiresAuth, notesRoutes);
+app.use("/api/notes", notesRoutes);
+
+app.use((req, res, next) => {
+    next(createHttpError(404, "Endpoint not found"));
+});
+
+console.log('post 404 middleware');
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
+
+console.log('post 500 middleware');
+
+export default app;
