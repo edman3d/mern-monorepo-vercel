e627fc5ca57baf648c86a0d4210e8299e58023b8
edman3d <ejsmajestic@gmail.com>
2025-10-16 07:43:53 -0400

    try to dynamically set REACT_APP_MONOREPO_FRONTEND_URL for preview build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,13 @@ import { connectToMongooseAndCache } from "./connectToDbAndCache";
 
 const app = express();
 
+if (process.env.VERCEL_ENV === 'preview') {
+    // sanitize branch name for URLs
+    const branch = process.env.VERCEL_GIT_COMMIT_REF!.replace(/\//g, '-');
+    const owner = process.env.VERCEL_GIT_REPO_OWNER;
+    process.env.REACT_APP_MONOREPO_FRONTEND_URL = `https://mern-monorepo-vercel-frontend-git-${branch}-${owner}.vercel.app`;
+}
+
 console.log("NODE_ENV: " + process.env.NODE_ENV);
 console.log("process.env.VERCEL_ENV: ", process.env.VERCEL_ENV);
 console.log("process.env.VERCEL_URL: ", process.env.VERCEL_URL);
